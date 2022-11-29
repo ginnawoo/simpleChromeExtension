9af170618dfae5b78fdfda1af580689ad7f85dd0
Ginna <ginnawoo@gmail.com>
2022-11-29 00:15:03 -0500

    test

--- popup.js
+++ popup.js
@@ -11,3 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
   },
     false)
 });
+
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  console.log(tabs[0].url);
+  console.log(tabs[0].title);
+});
