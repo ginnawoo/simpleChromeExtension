fe9f09b585121dc7bd3d90ba3b638ded88004fe4
Ginna <ginnawoo@gmail.com>
2022-11-29 00:27:58 -0500

    test

--- popup.js
+++ popup.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', function () {
   var checkPageButton = document.getElementById('clickIt');
   checkPageButton.addEventListener('click', function () {
 
-    chrome.tabs.getCurrent({
+    chrome.tabs.query({
       active: true,
       lastFocusedWindow: true
     }, function (tabs) {
-      var site = tabs.title;
-      var url = tabs.url;
+      var url = tabs[0].url;     //url
+      var site = tabs[0].title;   //title
       alert("Misinformation Level: 1" + " - Title: " + site + " - URL: " + url);
 
     });
